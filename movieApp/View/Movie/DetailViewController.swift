3d67491dc7c5d47e2ea09390a767194c6bccb8e8
Jane <piaojane@daum.net>
2021-06-28 16:32:17 +0900

    데이터 받아오자마자 맵핑하니까 이미지가 떴다.

--- movieApp/View/Movie/DetailViewController.swift
+++ movieApp/View/Movie/DetailViewController.swift
@@ -10,35 +10,34 @@ import Alamofire
 import Kingfisher
 
 class DetailViewController: UIViewController {
-
+    
     let detailApiCaller = DetailApiCaller()
     @IBOutlet weak var moviePoster: UIImageView!
-    
+    @IBOutlet weak var movieTitle: UILabel!
     
     var getId: Int?
     
     private var data: DetailResponse?
     
-    override func viewWillAppear(_ animated: Bool) {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
         if let i = getId {
             detailApiCaller.fetchData(id: i) { [weak self] response in
                 print(response)
                 self?.data = response
+                
+                if let p = self?.data?.poster_path {
+                    let url = URL(string:"https://image.tmdb.org/t/p/w500\(p)")
+                    print("url 주소는 \(url!)입니다.")
+                    self?.moviePoster.kf.setImage(with: url)
+                }
+                self?.movieTitle.text = self?.data?.title
+                print("영화 제목은 \(self?.movieTitle.text)입니다")
+                
             }
         }
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        if let p = data?.poster_path {
-            let url = URL(string:"https://image.tmdb.org/t/p/w500\(p)")
-            print("url 주소는 \(url)입니다.")
-            moviePoster.kf.setImage(with: url)
-        }
-        
-        
-        }
+}
 
-    }
-    
 
